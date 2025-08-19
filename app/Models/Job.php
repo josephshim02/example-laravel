ec984399816faf9f9ec42c6f9f67dc34a925be2a
Joseph Shim <josephshim02@gmail.com>
2025-08-19 13:02:08 +1000

    factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,14 @@
 
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model{
+    use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = [
