75775fd8fd185444d6e284a383f2fb7937206cac
Joseph Shim <josephshim02@gmail.com>
2025-08-20 00:05:27 +1000

    belongsTo and hasMany relationships. Essentially encodes relational database relationships into php. Allows you to write simple code in php which will be translated into an SQL query by eloquent!! :)

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -10,5 +10,8 @@ class Employer extends Model
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
 
-
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
