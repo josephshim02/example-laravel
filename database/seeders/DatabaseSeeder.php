5905a645eda8c8d8409667b1994b484d49da3ace
Joseph Shim <josephshim02@gmail.com>
2025-08-23 09:33:36 +1000

    15 seeders: Classes that call on factories to fill out database. This means we don't have to use tinker to fill out our databases, instead we can call "php artisan migrate:fresh --seed" to dump and populate the database.
    Alternatively call "php artisan db:seed" to just run the default seeder DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Job;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
             'email' => 'test@example.com',
         ]);
+        $this->call(JobSeeder::class);
+
     }
 }
